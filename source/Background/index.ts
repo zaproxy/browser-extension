d8a6abb74fd4e4f4543b1d16ee0cabbb16cbbdbf
karthikuj <karthikuj2001@gmail.com>
2023-05-24 23:06:18 +0530

    Adds code to report cookies
    Signed-off-by: karthikuj <karthikuj2001@gmail.com>

--- source/Background/index.ts
+++ source/Background/index.ts
@@ -18,8 +18,8 @@
  * limitations under the License.
  */
 import 'emoji-log';
-import Browser, {Runtime} from 'webextension-polyfill';
-import {ReportedStorage} from '../types/ReportedModel';
+import Browser, { Cookies, Runtime } from 'webextension-polyfill';
+import { ReportedStorage } from '../types/ReportedModel';
 
 console.log('ZAP Service Worker 👋');
 
@@ -36,6 +36,100 @@ function zapApiUrl(zapurl: string, action: string): string {
     return zapurl;
   }
   return `${zapurl}JSON/client/action/${action}/`;
+}
+
+async function getCookieTabUrl(
+  changeInfo: Cookies.OnChangedChangeInfoType
+): Promise<string> {
+  const getAllTabs = Browser.tabs.query({
+    currentWindow: true,
+  });
+  return new Promise((resolve, reject) => {
+    getAllTabs
+      .then((allTabs) => {
+        for (const tab of allTabs) {
+          if (!tab.url) {
+            continue;
+          }
+          const getAllCookiesForTab = Browser.cookies.getAll({ url: tab.url });
+          getAllCookiesForTab.then((cookies) => {
+            for (const cookie of cookies) {
+              if (
+                cookie.name === changeInfo.cookie.name &&
+                cookie.value === changeInfo.cookie.value &&
+                cookie.domain === changeInfo.cookie.domain &&
+                cookie.storeId === changeInfo.cookie.storeId
+              ) {
+                resolve(tab.url ? tab.url : (changeInfo.cookie.domain.startsWith('.')
+                  ? `http://${changeInfo.cookie.domain.substring(1)}`
+                  : `http://${changeInfo.cookie.domain}`));
+              }
+            }
+          });
+        }
+      })
+      .catch(() => {
+        console.log(
+          'Could not fetch the correct tab which invoked cookie change.'
+        );
+        reject(
+          changeInfo.cookie.domain.startsWith('.')
+            ? `http://${changeInfo.cookie.domain.substring(1)}`
+            : `http://${changeInfo.cookie.domain}`
+        );
+      });
+  });
+}
+
+async function reportCookies(
+  changeInfo: Cookies.OnChangedChangeInfoType,
+  zapurl: string,
+  zapkey: string
+): Promise<string> {
+  const { cookie } = changeInfo;
+  let cookieString = `${cookie.name}=${cookie.value}; path=${cookie.path}; domain=${cookie.domain}`;
+  if (cookie.expirationDate) {
+    cookieString = cookieString.concat(
+      `; expires=${new Date(cookie.expirationDate * 1000).toUTCString()}`
+    );
+  }
+  if (cookie.secure) {
+    cookieString = cookieString.concat(`; secure`);
+  }
+  if (cookie.sameSite === 'lax' || cookie.sameSite === 'strict') {
+    cookieString = cookieString.concat(`; SameSite=${cookie.sameSite}`);
+  }
+  if (cookie.httpOnly) {
+    cookieString = cookieString.concat(`; HttpOnly`);
+  }
+
+  const cookieUrl = await getCookieTabUrl(changeInfo).then((url) => url);
+  const repStorage = new ReportedStorage(
+    'Cookies',
+    '',
+    cookie.name,
+    '',
+    cookieString,
+    cookieUrl
+  );
+  const repStorStr: string = repStorage.toShortString();
+  if (!reportedStorage.has(repStorStr) && repStorage.url.startsWith('http')) {
+    const body = `objectJson=${encodeURIComponent(
+      repStorage.toString()
+    )}&apikey=${encodeURIComponent(zapkey)}`;
+
+    fetch(zapApiUrl(zapurl, 'reportObject'), {
+      method: 'POST',
+      body,
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+    });
+
+    reportedStorage.add(repStorStr);
+  }
+
+  return new Promise(() => "");
 }
 
 function handleMessage(
@@ -117,16 +211,30 @@ function onMessageHandler(
   return Promise.resolve(2);
 }
 
+function cookieChangeHandler(
+  changeInfo: Cookies.OnChangedChangeInfoType
+): void {
+  Browser.storage.sync
+    .get({
+      zapurl: 'http://localhost:8080/',
+      zapkey: 'hello',
+    })
+    .then((items) => {
+      reportCookies(changeInfo, items.zapurl, items.zapkey);
+    });
+}
+
 Browser.action.onClicked.addListener((_tab: Browser.Tabs.Tab) => {
   Browser.runtime.openOptionsPage();
 });
 
+Browser.cookies.onChanged.addListener(cookieChangeHandler);
 Browser.runtime.onMessage.addListener(onMessageHandler);
 
 Browser.runtime.onInstalled.addListener((): void => {
   console.emoji('🦄', 'extension installed');
   Browser.storage.sync.set({
     zapurl: 'http://localhost:8080/',
-    zapkey: 'not set',
+    zapkey: 'hello',
   });
 });
